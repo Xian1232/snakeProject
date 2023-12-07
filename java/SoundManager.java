190c7f78dacb9384db84c48438f3f226e22d1082
allanhy <36211173+allanhy@users.noreply.github.com>
2023-12-06 22:48:09 -0800

    Update SoundManager.java

--- java/SoundManager.java
+++ java/SoundManager.java
@@ -9,10 +9,12 @@ import android.media.SoundPool;
 import android.os.Build;
 import java.io.IOException;
 
+// Define the Audio interface
 interface Audio {
     void play();
 }
 
+// Concrete implementation for playing eat sound
 class EatSound implements Audio {
     private final SoundPool soundPool;
     private final int soundId;
@@ -28,6 +30,7 @@ class EatSound implements Audio {
     }
 }
 
+// Concrete implementation for playing crash sound
 class CrashSound implements Audio {
     private final SoundPool soundPool;
     private final int soundId;
@@ -43,6 +46,22 @@ class CrashSound implements Audio {
     }
 }
 
+class BombSound implements Audio {
+    private final SoundPool soundPool;
+    private final int soundId;
+
+    BombSound(SoundPool soundPool, int soundId) {
+        this.soundPool = soundPool;
+        this.soundId = soundId;
+    }
+
+    @Override
+    public void play() {
+        soundPool.play(soundId, 1, 1, 0, 0, 1);
+    }
+}
+
+// Context for Audio, allows switching between different audio implementations
 class AudioContext {
     private Audio audio;
 
@@ -57,11 +76,12 @@ class AudioContext {
     }
 }
 
+// SoundManager now uses the Audio and AudioContext classes
 public class SoundManager {
-
-    // for playing sound effects
     private SoundPool soundPool;
     private int eatSoundId = -1;
+    private int bombId = -1;
+
     private int crashSoundId = -1;
     private AudioContext audioContext;
 
@@ -69,11 +89,15 @@ public class SoundManager {
         initializeSoundPool(context);
         try {
             initializeSoundEffects(context);
-
         } catch (IOException e) {
-            // Error
+            // Handle error
         }
+
+        // Initialize the default audio context
+        audioContext = new AudioContext();
+        audioContext.setAudio(new EatSound(soundPool, eatSoundId)); // Set default to EatSound
     }
+
     private void initializeSoundPool(Context context) {
         // Initialize the SoundPool
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -99,19 +123,25 @@ public class SoundManager {
         descriptor = assetManager.openFd("get_apple.ogg");
         eatSoundId = soundPool.load(descriptor, 0);
 
+        descriptor = assetManager.openFd("bomb.wav");
+        bombId = soundPool.load(descriptor, 0);
+
         descriptor = assetManager.openFd("snake_death.ogg");
         crashSoundId = soundPool.load(descriptor, 0);
     }
 
     void playEatSound() {
-        // Play a sound
         audioContext.setAudio(new EatSound(soundPool, eatSoundId));
         audioContext.playAudio();
     }
 
     void playCrashSound() {
-        // Pause the game ready to start again
         audioContext.setAudio(new CrashSound(soundPool, crashSoundId));
+        audioContext.playAudio();
+    }
+
+    void playBombSound() {
+        audioContext.setAudio(new BombSound(soundPool, crashSoundId));
         audioContext.playAudio();
     }
 }
